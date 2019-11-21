28c469c1ec060c3d95e170a4b7129db0530f4cdb
Adam Selker <adamselker@gmail.com>
2019-11-21 00:26:30 -0500

    Making ngrams useful

--- most-common-ngrams.py
+++ most-common-ngrams.py
@@ -2,18 +2,33 @@
 
 import sys
 import pickle
+import matplotlib.pyplot as plt
 
-ngrams = pickle.load(open(sys.argv[1], "rb"))
+if __name__ == "__main__":
+    ngrams = pickle.load(open(sys.argv[1], "rb"))
 
-one_grams = [ng for ng in ngrams.items() if type(ng[0]) == str]
-two_grams = [ng for ng in ngrams.items() if type(ng[0]) == tuple and len(ng[0]) == 2]
-three_grams = [ng for ng in ngrams.items() if type(ng[0]) == tuple and len(ng[0]) == 3]
-print(len(three_grams))
+    one_grams = [ng for ng in ngrams.items() if type(ng[0]) == str]
+    two_grams = [
+        ng for ng in ngrams.items() if type(ng[0]) == tuple and len(ng[0]) == 2
+    ]
+    three_grams = [
+        ng for ng in ngrams.items() if type(ng[0]) == tuple and len(ng[0]) == 3
+    ]
 
-one_grams = sorted(one_grams, key=lambda x: -x[1])
-two_grams = sorted(two_grams, key=lambda x: -x[1])
-three_grams = sorted(three_grams, key=lambda x: -x[1])
+    one_grams = sorted(one_grams, key=lambda x: -x[1])
+    two_grams = sorted(two_grams, key=lambda x: -x[1])
+    three_grams = sorted(three_grams, key=lambda x: -x[1])
 
-print(one_grams[:100])
-print(two_grams[:100])
-print(three_grams[:100])
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    assert len(alphabet) == 26
+
+    letter_1s_lower = [w for w in one_grams if w[0] in alphabet]
+    letter_1s_upper = [
+        w for w in one_grams if w[0].lower() in alphabet and w[0].isupper()
+    ]
+
+    print(letter_1s_lower)
+    print(letter_1s_upper)
+
+    plt.barh(range(26), letter_1s_lower)
+    plt.show()
