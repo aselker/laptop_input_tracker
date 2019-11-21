682c6f7b1d17d4d374f6de6001258a2c1ae37159
Adam Selker <adamselker@gmail.com>
2019-11-21 10:50:10 -0500

    Add a heatmap that doesn't suck

--- mouse-visualizer.py
+++ mouse-visualizer.py
@@ -21,6 +21,35 @@ def show_streamlines(diffs):
     plt.streamplot(xs, ys, xvels, yvels, linewidth=magnitude, density=3, arrowstyle="-")
 
 
+def show_heatmap(diffs):
+    magnitude = np.linalg.norm(diffs, axis=2)
+    magnitude = np.array(gaussian(magnitude, sigma=50))
+
+    def color_map(x):
+        x = x - np.amin(x)
+        x = x / np.amax(x)
+
+        fudge_up = 0.8
+        fudge_down = 1.4
+        x = x * (1 + fudge_up + fudge_down)
+        x = np.clip(x, 0, 1)
+
+        start_rgb = np.array([135, 210, 206]) / 255
+        end_rgb = np.array([255, 255, 255]) / 255
+
+        background = np.array([np.full_like(magnitude, s) for s in start_rgb])
+        foreground = np.array([np.full_like(magnitude, s) for s in end_rgb])
+
+        mixed = (x * foreground) + ((1 - x) * background)
+
+        return np.transpose(mixed)
+
+    colors = color_map(magnitude)
+    print(colors.shape)
+
+    plt.imshow(colors)
+
+
 screen_size = (3200, 1800)
 
 if __name__ == "__main__":
@@ -37,7 +66,7 @@ if __name__ == "__main__":
 
     total_diffs = gaussian(total_diffs, sigma=20, multichannel=True)
 
-    clip_amt = 50
+    clip_amt = 50  # TODO: Adjust this
     total_diffs = total_diffs[clip_amt:-clip_amt, clip_amt:-clip_amt, :]
 
     # amplitudes = np.linalg.norm(total_diffs, axis=2)
@@ -46,4 +75,6 @@ if __name__ == "__main__":
 
     plt.figure(figsize=(32, 18))
     show_streamlines(total_diffs)
-    plt.savefig("streamlines.pdf")
+    show_heatmap(total_diffs)
+    # plt.savefig("streamlines.pdf")
+    plt.show()
