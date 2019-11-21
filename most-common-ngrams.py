358b73eb99c2b6470a24cfcbb8ac0bbce0bb4d9c
Adam Selker <adamselker@gmail.com>
2019-11-21 01:13:41 -0500

    Finish horizontal freq bar graph

--- most-common-ngrams.py
+++ most-common-ngrams.py
@@ -4,6 +4,14 @@ import sys
 import pickle
 import matplotlib.pyplot as plt
 
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+assert len(alphabet) == 26
+
+
+def is_letter(l):
+    return l and (l.lower() in alphabet)
+
+
 if __name__ == "__main__":
     ngrams = pickle.load(open(sys.argv[1], "rb"))
 
@@ -19,16 +27,21 @@ if __name__ == "__main__":
     two_grams = sorted(two_grams, key=lambda x: -x[1])
     three_grams = sorted(three_grams, key=lambda x: -x[1])
 
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    assert len(alphabet) == 26
+    letter_1s_lower = [w for w in one_grams if is_letter(w[0]) and w[0].islower()]
+    letter_1s_upper = [w for w in one_grams if is_letter(w[0]) and w[0].isupper()]
 
-    letter_1s_lower = [w for w in one_grams if w[0] in alphabet]
-    letter_1s_upper = [
-        w for w in one_grams if w[0].lower() in alphabet and w[0].isupper()
-    ]
+    y_pos = range(26, 0, -1)
+    right_bars = [l[1] for l in letter_1s_lower]
+    left_bars = [-ngrams.get(r[0].upper(), 0) for r in letter_1s_lower]
 
-    print(letter_1s_lower)
-    print(letter_1s_upper)
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
 
-    plt.barh(range(26), letter_1s_lower)
-    plt.show()
+    # ax.barh(range(26, 0, -1), [l[1] for l in letter_1s_lower])
+    ax.barh(y_pos, right_bars)
+    ax.barh(y_pos, left_bars)
+
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels([l[0] for l in letter_1s_lower])
+
+    plt.savefig("letter_freqs.pdf")
